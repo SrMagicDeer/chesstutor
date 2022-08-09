fd53a101da27a422b5a84c743dfbff465b5030a7
SrMagicDeer <pbilinkis5@gmail.com>
2022-08-09 07:13:53 +0300

    Чутка продвинулся в практичности. Пока основываюсь на ручном обновлении базы и одном уровне сложности. Проблема - не могу структурировать логистику лайновых кнопок на этапе решения задач (следующая задача, предыдущая, подсказка, возврат в меню)

--- bot.py
+++ bot.py
@@ -1,39 +1,48 @@
 import telebot
+import config
 from telebot import types
 
-bot = telebot.TeleBot('5348803276:AAH8610B53HBOTOuxM4IqTd62OjNyJlLBTA')
+bot = telebot.TeleBot(config.token)
 
-
-# Текст примерный, сразу говорю, написан чисто для теста предложения вариантов ботом
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Да")
-    markup.add(btn1)
+    btn_start = types.KeyboardButton("Погнали")
+    markup.add(btn_start)
+
     mess = f'Привет, <b>{message.from_user.first_name}</b>. Я твой наставник по шахматам в виде бота. Я буду давать тебе шахматные задачи, готов начать?'
     bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
-def level(message):
-    if message.text == "Да":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn2 = types.KeyboardButton("Начинающий")
-        btn3 = types.KeyboardButton("Продвинутый")
-        btn4 = types.KeyboardButton("Мастер спорта")
-        markup.add(btn2, btn3, btn4)
-        mess = f'Тогда, для начала, выбери <b>уровень сложности</b>, который тебе подходит'
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+def tasks(message):
+    if message.text == "Погнали":
 
-    elif message.text == "Начинающий":
-        bot.send_message(message.chat.id, "Начинающий")
+        markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_accept = types.KeyboardButton("Я все понял, давай к делу")
+        markup_menu.add(btn_accept)
 
-    elif message.text == "Продвинутый":
-        bot.send.message(message.chat.id, "Продвинутый")
+        mess = f'Отлично, рад слышать! Моя база данных задач обновляется каждый день, <b>ныняшняя версия: 0.1</b> Задачи усложняются по ходу решения. Ты можешь пропустить задачу, вернуться к предыдущей, попросить подсказку или вернуться в это меню и начать все заново. Чтобы ответить на задачу необходимо написать ответ в формате "e2-e4" (пример) и просто отправить мне. Я скажу, если тыответил правильно или допустил ошибку. Приятного время препровождения!'
+        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup_menu)
 
-    elif message.text == "Мастер спорта":
-        bot.send_message(message.chat.id, "Мастер спорта")
+    elif message.text == "Я все понял, давай к делу":
+
+        keyboard_1 = types.InlineKeyboardMarkup()
+        callback_button_1 = types.InlineKeyboardButton(text="Кнопка1", callback_data="knopka1")
+        keyboard_1.add(callback_button_1)
+
+        task_1 = 'https://skr.sh/sFLDGEHa3XV?a'
+        bot.send_photo(message.chat.id, task_1, 'Задача #1', reply_markup=keyboard_1)
+        bot.register_next_step_handler(message, tasks)
+
+    elif message.text == "a2-a1":
+        task_1_done = 'https://skr.sh/sFLdRFxiDgV'
+        bot.send_photo(message.chat.id, task_1_done, 'Отлично! Ты правильно решил задачу, переходи к следующей!')
+
+    else:
+        task_1 = 'https://skr.sh/sFLDGEHa3XV?a'
+        bot.send_photo(message.chat.id, task_1, 'Ты допустил ошибку, попробуй еще раз или пропусти задачу')
 
 
 bot.polling(none_stop=True)
